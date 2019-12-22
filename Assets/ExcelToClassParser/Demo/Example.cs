7f9d4066a6060b439c6de822fb107ae6e2e11a42
limbyeongsoo <lbs0718@gmail.com>
2019-12-22 14:12:27 +0900

    add new project.

---
+++ Assets/ExcelToClassParser/Demo/Example.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Text;
+
+public class Example : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        string text = File.ReadAllText(Application.dataPath + "/ExcelToClassParser/Demo/Json/CharacterInfo.json");
+        Dictionary<string, CharacterInfo> charInfos = JsonToClass.DeserializeStringKeyDic<CharacterInfo>("Id", text);
+        Dictionary<int, CharacterInfo> charInfosInt = JsonToClass.DeserializeIntKeyDic<CharacterInfo>("Power", text);
+        foreach (KeyValuePair<string, CharacterInfo> pair in charInfos)
+        {
+            FieldInfo[] fields = pair.Value.GetType().GetFields();
+            string infoStr = "";
+            for (int i = 0; i < fields.Length; i++)
+            {
+                infoStr += fields[i] + " + ";
+            }
+            Debug.Log("Key: " + pair.Key + " Infos: " + infoStr);
+        }
+    }
+}
+public class CharacterInfo
+{
+    public string Id;
+    public string Name;
+    public string Race;
+    public int Level;
+    public int Power;
+    public int Agility;
+    public int Charisma;
+    public int Intellect;
+    public string[] PastTimes;
+    public int[] Skills;
+}
---
+++ Assets/ExcelToClassParser/JsonToClass/JsonToClass.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections;
+using System.Reflection;
+using System.Collections.Generic;
+using UnityEngine;
+using JsonFx.Json;
+
+public class JsonToClass : MonoBehaviour
+{
+
+    public static T[] Deserialize<T>(string json)
+    {
+        T[] parsedClass = JsonReader.Deserialize<T[]>(json);
+        return parsedClass;
+    }
+    public static Dictionary<string, T> DeserializeStringKeyDic<T>(string keyValue, string json)
+    {
+        //keyvalue 에 들어있는 실제 값을 키로 사용하여 dictionary에 넣는다. 중복될 경우가 없어야 한다.    
+        T[] parsedClass = JsonReader.Deserialize<T[]>(json);
+        Dictionary<string, T> dic = new Dictionary<string, T>();
+        for (int i = 0; i < parsedClass.Length; i++)
+        {
+            object o = parsedClass[i].GetType().GetField(keyValue);
+            if (o == null)
+                o = parsedClass[i].GetType().GetProperty(keyValue).GetValue(parsedClass[i], null);
+            else
+                o = parsedClass[i].GetType().GetField(keyValue).GetValue(parsedClass[i]);
+            string currKey = (string)o;
+            if (dic.ContainsKey(currKey))
+                Debug.LogError("Json Parsing Error: " + keyValue + "'s key ->" + currKey + " Already exist. Must use Different Key Value");
+            dic.Add(currKey, parsedClass[i]);
+        }
+        return dic;
+    }
+    public static Dictionary<int, T> DeserializeIntKeyDic<T>(string keyValue, string json)
+    {
+        //keyvalue 에 들어있는 실제 값을 키로 사용하여 dictionary에 넣는다. 중복될 경우가 없어야 한다.        
+        T[] parsedClass = JsonReader.Deserialize<T[]>(json);
+        Dictionary<int, T> dic = new Dictionary<int, T>();
+        for (int i = 0; i < parsedClass.Length; i++)
+        {
+            object o = parsedClass[i].GetType().GetField(keyValue);
+            if (o == null)
+                o = parsedClass[i].GetType().GetProperty(keyValue).GetValue(parsedClass[i], null);
+            else
+                o = parsedClass[i].GetType().GetField(keyValue).GetValue(parsedClass[i]);
+            int currKey = (int)o;
+            if (dic.ContainsKey(currKey))
+                Debug.LogError("Json Parsing Error: " + keyValue + "'s key ->" + currKey + " Already exist. Must use Different Key Value");
+            dic.Add(currKey, parsedClass[i]);
+        }
+        return dic;
+    }
+
+}
